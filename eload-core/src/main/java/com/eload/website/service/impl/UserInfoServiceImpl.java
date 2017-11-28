9e69dfe8b9aba6db45e64113d2d7b33f9195746c
xuyanCavs <haharod@163.com>
2017-11-29 00:22:02 +0800

    增加了UserInfo和Account的service和Controller

--- eload-core/src/main/java/com/eload/website/service/impl/UserInfoServiceImpl.java
+++ eload-core/src/main/java/com/eload/website/service/impl/UserInfoServiceImpl.java
@@ -1,6 +1,18 @@
 package com.eload.website.service.impl;
 
+import com.eload.dao.UserInfoMapper;
+import com.eload.pojo.UserInfo;
 import com.eload.website.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserInfoServiceImpl implements UserInfoService{
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @Override
+    public UserInfo getUserInfo(long l) {
+        return userInfoMapper.selectByPrimaryKey(l);
+    }
 }
